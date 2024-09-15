ab69fcc06f3a2b89989d979231dfd8c5b567813c
CHAO201030 <2227685463@qq.com>
2024-09-16 00:00:41 +0800

    update sth

--- damn-project/netdisk_v5.0/client/H_client.h
+++
@@ -1,8 +0,0 @@
-#ifndef __H_CLIENT_H__
-#define __H_CLIENT_H__
-
-#include "H_head.h"
-#include "H_net.h"
-#include "H_trans_file.h"
-
-#endif
--- damn-project/netdisk_v5.0/client/H_head.h
+++
@@ -1,6 +0,0 @@
-#ifndef __H_HEAD_H__
-#define __H_HEAD_H__
-
-#include <func.h>
-
-#endif
